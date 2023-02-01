f40b1ffd4a12dc3d1b5ba1d69e3a8cb6174bd065
mtl11 <mtl11@email.arizona.edu>
2023-02-01 01:07:40 -0800

    Delete unused auth screens

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import StartScreen from "./pages/authScreens/StartScreen";
 import { useFonts } from "expo-font";
 import SignupScreen from "./pages/authScreens/SignupScreen";
-import SetupScreen from "./pages/authScreens/SetupScreen";
 import ProfileSettingsScreen from "./pages/profileScreens/ProfileSettingsScreen";
-import ArtistSetupScreen from "./pages/authScreens/ArtistSetupScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ArtistsocialsScreen from "./pages/authScreens/ArtistsocialsScreen";
 import TabNav from "./components/TabNav";
 import ForgetPasswordScreen from "./pages/authScreens/ForgetPasswordScreen";
-import VenueSetupScreen from "./pages/authScreens/VenueSetupScreen";
 import PersonalInfoScreen from "./pages/profileScreens/PersonalInfoScreen";
 import PersonalSecurityScreen from "./pages/profileScreens/PasswordSecurityScreen";
 import SearchArtistProfile from "./pages/searchScreens/SearchArtistProfile";
@@ -43,28 +37,8 @@ export default function App() {
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="VenueSetup"
-          component={VenueSetupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
           name="Signup"
           component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Setup"
-          component={SetupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ArtistSetup"
-          component={ArtistSetupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ArtistSocial"
-          component={ArtistsocialsScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
